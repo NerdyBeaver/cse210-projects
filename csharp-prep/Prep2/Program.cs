e04a47f3a8d5669f7a571099b83b64e36df725db
NerdyBeaver <all24057@byui.edu>
2025-04-24 13:11:27 -0600

    Corrected grade input so it must be an integer, completed stretch requirement for the letter grade calculator.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your grade percentage? ");
+        Console.Write("What is your grade percentage? Round to the neares whole number. ");
         string grade = Console.ReadLine();
-        float percentGrade = float.Parse(grade);
+        int percentGrade = int.Parse(grade);
 
         string letter = "";
 
@@ -31,7 +31,30 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine($"Your grade is: {letter}");
+
+        string sign = "";
+
+        if (93 > percentGrade && percentGrade >= 60)
+        {
+            if (grade[1] >=7)
+            {
+                sign = "+";  
+            }         
+        
+            else if (grade[1] < 3)
+            {
+                sign = "-";
+            }
+
+            else sign = "";
+        }
+
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
 
         if (percentGrade > 70)
         {
