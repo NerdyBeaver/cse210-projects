d09a2ae0d972cb71c240dd5a58d181b86b96fac9
NerdyBeaver <all24057@byui.edu>
2025-04-24 12:32:35 -0600

    Completed core requirements for letter grade-determining program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        float percentGrade = float.Parse(grade);
+
+        string letter = "";
+
+        if (percentGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentGrade > 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("No worries! You'll ace it next time!");
+        }
     }
 }
