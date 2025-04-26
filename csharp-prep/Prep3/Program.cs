ea63aaa08df1f8b5c4660c057ae018c165e3f740
NerdyBeaver <all24057@byui.edu>
2025-04-25 21:07:53 -0600

    Completed step 1 of Core Requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string inputNumber = Console.ReadLine();
+        int magicNumber = int.Parse(inputNumber);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessNumber = int.Parse(guess);
+
+        if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (guessNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+
+
+
+
+
+
+
+
     }
 }
