f5fd22a15d8d0c74076e7f6aa607bdf74d3bfc99
NerdyBeaver <all24057@byui.edu>
2025-04-27 20:20:32 -0600

    Completed all Core Requirements.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type O when finished.");
+
+        List<float> numbers = new List<float>();
+
+        float answer = -1;
+
+        float average = 0;
+
+        float total = 0;
+
+        float largest = 0;
+
+        while (answer != 0)
+        {
+            Console.Write("Enter number: ");
+
+            answer = float.Parse(Console.ReadLine());
+
+            numbers.Add(answer);
+        }
+
+        foreach (float number in numbers)
+        {
+            total += number;
+
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        average = total / numbers.Count;
+
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
