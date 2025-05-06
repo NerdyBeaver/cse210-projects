c61751e3584e54b90c0a018bce0264fe548b2319
NerdyBeaver <all24057@byui.edu>
2025-05-05 21:42:45 -0600

    Created Resume.cs file, created code for resume class, finished requirements for Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,9 +7,23 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Apple";
-        job1._startYear = 2003;
-        job1._endYear = 2025;
-        job1.DisplayJobInfo();
+        job1._startYear = 1999;
+        job1._endYear = 2012;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Janitor";
+        job2._company = "Toys-R-Us";
+        job2._startYear = 2012;
+        job2._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._name = "Kevin";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResumeInfo();
+
 
     }
 }
