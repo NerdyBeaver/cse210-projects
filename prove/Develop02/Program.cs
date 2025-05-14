384967a7fb02c915f311cf5d6f392d5368dcafe5
NerdyBeaver <all24057@byui.edu>
2025-05-13 21:23:47 -0600

    Gave real prompts in the Prompt class, made menu and main program in Program class.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,7 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
+        int choice = 0;
 
+        Journal journal = new Journal();
 
+        while (choice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices (just type the integer): ");
+            Console.WriteLine("1. Write an entry");
+            Console.WriteLine("2. Display entries");
+            Console.WriteLine("3. Load a file to display");
+            Console.WriteLine("4. Save the current entries to a file");
+            Console.WriteLine("5. Quit the program");
+
+            
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+            case 1: 
+                journal.AddEntry();
+                break;
+            case 2:
+                journal.Display();
+                break;
+            case 3:
+                journal.LoadFromFile();
+                break;
+            case 4:
+                journal.SaveToFile();
+                break;
+            case 5: //Simply here so I can have a default that won't print an error when the user enters 5
+                choice = 5;
+                break;
+            default:
+                Console.WriteLine("Not a valid option. Try again. ");
+                break;
+            }
+        }
     }
 }
--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -6,11 +6,16 @@ public class Prompt
 {
     List<string> prompts = new List<string>
         {
-            "Foo",
-            "Bar",
-            "Baz",
-            "Qux",
-            "Quux"
+            "What am I most grateful for today? ",
+            "What did I do to help someone today? ",
+            "What is one thing that brought joy to me today or made me smile? ",
+            "What did I mess up on today that I can be better at tomorrow? ",
+            "What could I have done to have been kinder to others today? ",
+            "What was the best part of my day? ",
+            "What did I learn today? ",
+            "What is the strongest emotion I felt today? ",
+            "Who made the biggest impact on my day today? ",
+            "What am I most proud of doing today? "
         };
      
 
@@ -18,7 +23,7 @@ public class Prompt
     {
         Random randomGenerator = new Random();
 
-        int random_number = randomGenerator.Next(1,6);
+        int random_number = randomGenerator.Next(1,11);
         string prompt = prompts[random_number];
         Console.WriteLine(prompt);
 
