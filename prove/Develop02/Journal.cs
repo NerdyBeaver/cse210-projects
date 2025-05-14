9920e4e7faf068936060d97e84e46e0dabab95ee
NerdyBeaver <all24057@byui.edu>
2025-05-13 21:25:46 -0600

    fixed formatting in Display function

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -24,9 +24,9 @@ public class Journal
         {
             foreach (Entry listed_entry in _entries) //Goes through each entry in _entries and prints the journal entry 
             {
+                Console.WriteLine($"Date: {listed_entry._date} — Prompt: {listed_entry.prompt}");
                 Console.WriteLine($"{listed_entry._entry}");
                 Console.WriteLine("");
-                Console.WriteLine($"Date: {listed_entry._date} — Prompt: {listed_entry.prompt}");
             }
         }
 
