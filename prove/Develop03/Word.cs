db75eeb92e4a0fedc9fc6d6131b009af6562b941
NerdyBeaver <all24057@byui.edu>
2025-05-28 23:05:03 -0600

    Finished Word class - filled out behaviors, added constructor

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -5,16 +5,30 @@ public class Word
     private string _text;
     private bool _isHidden;
 
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
     public void Hide()
     {
-
+        _isHidden = true;
     }
-    public bool IsHidden()
+    public bool GetIsHidden()
     {
-
+        return _isHidden;
     }
     public string GetRenderedText()
     {
-        
+        if (_isHidden)
+        {
+            return "____"; // How to get same number of dashes as letters?
+        }
+
+        else
+        {
+            return _text;
+        }
     }
 }
