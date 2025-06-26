059afb6de3ec2d5747d96676fa80a6e8b559c8aa
NerdyBeaver <all24057@byui.edu>
2025-06-25 18:55:53 -0600

    finished /mathassignments class

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -1,7 +1,19 @@
-public class MathAssigment : Assignment
+using System;
+
+public class MathAssignment : Assignment
 {
     private string _textbookSection;
     private string _problems;
 
-    public MathAssigment(string textbookSection, string problems) : base(studentName, topic);
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
 }
