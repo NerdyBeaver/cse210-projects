35adbd143e8e6408096657246fbd99392184e622
NerdyBeaver <all24057@byui.edu>
2025-06-25 18:46:26 -0600

    started MathAssignment class, created assignment class

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,7 @@
+public class MathAssigment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssigment(string textbookSection, string problems) : base(studentName, topic);
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Greg", "History");
+        Console.WriteLine(assignment1.GetSummary());
     }
 }
