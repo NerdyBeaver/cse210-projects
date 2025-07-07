56fab8f7770ecf045cc896fa54c274410e8c3f4c
NerdyBeaver <all24057@byui.edu>
2025-07-07 17:05:37 -0600

    fixed code to correct program.cs file.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+
+class Activity
+{
+    // Base class for activities; add common properties or methods if needed
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflection Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+
+            Activity activity = null; 
+
+            switch (choice)
+            {
+                case "1":
+                    BreathingActivity breathingActivity = new BreathingActivity();
+                    breathingActivity.RunActivity();
+                    break;
+                case "2":
+                    ReflectionActivity reflectionActivity = new ReflectionActivity();
+                    reflectionActivity.RunActivity();
+                    break;
+                case "3":
+                    ListingActivity listingActivity = new ListingActivity();
+                    listingActivity.RunActivity();
+                    break;
+                case "4":
+                    Console.WriteLine("Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Press any key to try again.");
+                    Console.ReadKey();
+                    break;
+            }
+        }
     }
 }
